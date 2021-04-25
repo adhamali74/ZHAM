a3717dea0fe668b09e38b313f42420c1c3d41371
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2021-04-25 21:04:10 +0200

    254
    commited

--- app/src/main/java/com/example/loginregistrationservice/ui/MainActivity.kt
+++ app/src/main/java/com/example/loginregistrationservice/ui/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.loginregistrationservice.R
+import com.example.loginregistrationservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         supportActionBar?.hide()
 //        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
@@ -28,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         Intent(this, ForgetPasswordActivity::class.java).apply {
             startActivity(this)
         }
+
     }
 
 }
